1b1aa808aa1120b198f99411e9d8b3130d6df1f9
enzo <enzogrledo@gmail.com>
2026-09-05 18:31:09 -0300

    implement interactive client protocol

--- src/Client.java
+++ src/Client.java
@@ -2,24 +2,32 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         try(
-            Socket client = new Socket("localHost", 5000);
+            Socket client = new Socket("localhost", 5000);
         ) {
             DataOutputStream output = new DataOutputStream(client.getOutputStream());
             DataInputStream input = new DataInputStream(client.getInputStream());
 
-            output.writeUTF("echo Hi server");
-            output.flush();
+            while(true) {
+                String message = scanner.nextLine();
+                output.writeUTF(message);
+                output.flush();
 
-            String response = input.readUTF();
+                if (message.equals("quit")) {
+                    break;
+                }
 
-            System.out.println("Resposta: " + response);
+                String response = input.readUTF();
 
+                System.out.println("Response: " + response);
+            }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
