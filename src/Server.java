b80ebb593173661e1d2e2ea9fd1554676579e973
enzo <enzogrledo@gmail.com>
2026-09-05 18:52:03 -0300

    add thread pool for lient handlers

--- src/Server.java
+++ src/Server.java
@@ -1,11 +1,14 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
 
     public static void main(String[] args) {
-
+        ExecutorService executor =
+                Executors.newFixedThreadPool(4);
         try(ServerSocket server = new ServerSocket(5000)) {
 
             System.out.println("Server initialized successfully.");
@@ -13,10 +16,7 @@ public class Server {
             while (true) {
                 Socket client = server.accept();
 
-                ClientHandler handler = new ClientHandler(client);
-
-                Thread thread = new Thread(handler);
-                thread.start();
+                executor.submit(new ClientHandler(client));
             }
 
         } catch (IOException ex) {
